f36e6158f40b87ab870ed60b9704563642603416
skroy <skroychan@gmail.com>
2017-12-17 00:35:01 +0400

    Added command for forced reverse and option to reverse parentheses

--- handlers/reverser.py
+++ handlers/reverser.py
@@ -1,10 +1,13 @@
 from api.method import send_message
-from utils import message_utils
+from utils import filters, message_utils
+from utils.debug import debug_with_forward
 
-import random
+import random, re
 
 probability = 0.005
 max_length = 40  # 0 - no limit
+reverse_parentheses = True  # e.g. (hello) => (olleh) instead of )olleh(
+# todo: map
 
 
 def handler(update):
@@ -12,7 +15,16 @@ def handler(update):
     text = message_utils.get_text_or_caption(message)
     if not text:
         return
-    if (max_length <= 0 or len(text) <= max_length) and random.random() <= probability:
+    params = filters.command(text, 'reverse', split=False)
+    if params is not None and len(params) == 1:
+        text = params[0]
+
+    if params is not None or ((max_length <= 0 or len(text) <= max_length) and random.random() <= probability):
+        if reverse_parentheses:
+            text = re.sub('\((.*)\)', ')\g<1>(', text)
         text = text[::-1]
-        send_message(message.chat.id, text.encode('utf-8'))
-        return True
+        try:
+            send_message(message.chat.id, text.encode('utf-8'))
+            return True
+        except UnicodeDecodeError as e:
+            debug_with_forward(message, 'Exception: ' + repr(e))
