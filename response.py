d7d6b1ea1046eb233ae57dfd1b8becd7c1772d82
skroy <skroychan@gmail.com>
2021-08-21 14:19:34 +0400

    Upgrade to python 3

--- response.py
+++ response.py
@@ -1,29 +1,29 @@
 from config import BASE_URL
 from api.entity import File
 
-import httplib, mimetypes, urlparse, uuid, urllib, urllib2, logging
+import http.client, mimetypes, urllib, uuid, logging
 
 
 def send_response(method, data):
     try:
-        if any(isinstance(x, File) and x.is_binary for x in data.itervalues()):
+        if any(isinstance(x, File) and x.is_binary for x in data.values()):
             post_multipart(BASE_URL + method, data)
         else:
-            urllib2.urlopen(BASE_URL + method, urllib.urlencode(data)).read()
+            urllib.request.urlopen(BASE_URL + method, urllib.parse.urlencode(data, True).encode('utf-8')).read()
         return True
-    except Exception, e:
+    except Exception as e:
         logging.info('Data:\n' + str(data) + '\nException:\n' + repr(e))
         return False
 
 
 def post_multipart(url, data):
-    parts = urlparse.urlparse(url)
+    parts = urllib.parse.urlparse(url)
     host = parts[1]
     selector = parts[2]
     limit = '----------' + uuid.uuid4().hex
     clrf = '\r\n'
     l = []
-    for (key, value) in data.items():
+    for (key, value) in list(data.items()):
         l.append('--' + limit)
         if isinstance(value, File):
             l.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (key, value.name))
@@ -38,7 +38,7 @@ def post_multipart(url, data):
     l.append('')
     body = clrf.join(l)
     content_type = 'multipart/form-data; boundary=%s' % limit
-    h = httplib.HTTPS(host)
+    h = http.client.HTTPS(host)
     h.putrequest('POST', selector)
     h.putheader('content-type', content_type)
     h.putheader('content-length', str(len(body)))
