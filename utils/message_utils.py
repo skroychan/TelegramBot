1e9801078bd55c78e6d34412ba132a9e9b64aafa
skroy <skroychan@gmail.com>
2023-07-26 10:26:57 +0400

    encoding

--- utils/message_utils.py
+++ utils/message_utils.py
@@ -1,11 +1,11 @@
-from api.entity import Message
+﻿from api.entity import Message
 
 
 def get_text(message):
-    if message and isinstance(message, Message):
-        return message.text
+	if message and isinstance(message, Message):
+		return message.text
 
 
 def get_text_or_caption(message):
-    if message and isinstance(message, Message):
-        return message.text or message.caption
+	if message and isinstance(message, Message):
+		return message.text or message.caption
